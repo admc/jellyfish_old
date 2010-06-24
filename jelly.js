374fc8ed75f57a62846bc9c75f54f14e585075af
Adam Christian <adam.christian@gmail.com>
2010-06-24 02:48:23 -0700

    Adding an admin interface with bespin, cleaning up code, and adding wmlite to navigate the page

--- jelly.js
+++ jelly.js
@@ -3,40 +3,60 @@ var sys = require("sys")
   ,url = require("url")
   ,path = require('path')
   ,paperboy = require('./lib/paperboy')
-  ,WEBROOT = path.join(path.dirname(__filename), 'static');
+  ,WEBROOT = path.join(path.dirname(__filename), 'static')
+  ,ADMIN = path.join(path.dirname(__filename), 'admin');
 
+//Global registry of browsers
 var tentacles = {};
 
-var requestHandler = function (clientRequest, clientResponse) {
-  var req = clientRequest;
-  var res = clientResponse;
+//Assign jobs to available tentacles that match
+var assign = function(who, what) {
+  var matches = {};
+
+  for (var nemato in tentacles) {
+    var obj = tentacles[nemato];
+    //needs to be expanded to match via url or title regex
+    if (obj.title.indexOf(who) != -1) {
+       obj.queue.push(what);
+       sys.puts('Assigned: '+what+' to: '+obj.title+', nemato: '+nemato);
+       matches[nemato] = obj;
+    }
+  }
+  return matches;
+}
+
+//Finish the request
+var finish = function(req, res, data) {
+  var dataString = JSON.stringify(data);
+  req.headers['content-type'] = 'application/json';
+  req.headers['content-length'] = dataString.length;
+  res.writeHead(200, req.headers);
+  res.write(dataString);
+  res.end();
+}
+
+var requestHandler = function (req, res) {
   var ip = req.connection.remoteAddress;
-  var uri = url.parse(clientRequest.url);
+  var uri = url.parse(req.url);
   if (uri.port == undefined) {
     uri.port = {"http:":80,"https:":443}[uri.protocol]
   }
-  var pathname = uri.search ? uri.pathname + uri.search : uri.pathname
-  //sys.puts(pathname)
-  
+  var pathname = uri.search ? uri.pathname + uri.search : uri.pathname;
+
   //communcation loop
   if (pathname.indexOf('jelly-poll') != -1) {
     var tid = pathname.split("=")[1];
     var data = '';
-    if (tentacles[tid].queue.length != 0){
+    if (tentacles[tid] && (tentacles[tid].queue.length != 0)){
       data = tentacles[tid].queue.shift();
     }
-    var dataString = JSON.stringify(data);
-    req.headers['content-type'] = 'application/json';
-    req.headers['content-length'] = dataString.length;
-    clientResponse.writeHead(200, req.headers);
-    clientResponse.write(dataString);
-    clientResponse.end();
+    finish(req, res, data);
   }
   //register frames
   else if (pathname.indexOf('jelly-net/add') != -1) {
     var newDate = new Date;
     var id = newDate.getTime();
-    clientRequest.addListener("data", function (chunk) {
+    req.addListener("data", function (chunk) {
       sys.puts("Adding tentacle, "+id+" : "+chunk);
       eval("var tObj="+chunk);
       tObj.active = true;
@@ -45,26 +65,16 @@ var requestHandler = function (clientRequest, clientResponse) {
     })
     var data = {};
     data.id = id;
-    var dataString = JSON.stringify(data);
-    req.headers['content-type'] = 'application/json';
-    req.headers['content-length'] = dataString.length;
-    clientResponse.writeHead(200, req.headers);
-    clientResponse.write(dataString);
-    clientResponse.end();
+    finish(req, res, data);
   }
   //unregister frames
   else if (pathname.indexOf('jelly-net/rm') != -1) {
     var tid = pathname.split("=")[1];
     sys.puts("Removing tentacle: "+tid+": "+JSON.stringify(tentacles[tid]));
     delete tentacles[tid];
-    
+
     var data = "OK";
-    var dataString = JSON.stringify(data);
-    req.headers['content-type'] = 'application/json';
-    req.headers['content-length'] = dataString.length;
-    clientResponse.writeHead(200, req.headers);
-    clientResponse.write(JSON.stringify(dataString));
-    clientResponse.end();
+    finish(req, res, data);
   }
   //Serve up static files
   else if (pathname.indexOf('jelly-serv') != -1) {
@@ -75,19 +85,10 @@ var requestHandler = function (clientRequest, clientResponse) {
 
     paperboy
     .deliver(WEBROOT, req, res)
-    .before(function() {
-      //sys.puts('About to deliver: '+req.url);
-    })
-    .after(function() {
-      //sys.puts('Delivered: '+req.url);
-    })
-    .error(function() {
-      //sys.puts('Error delivering: '+req.url);
-    })
     .otherwise(function() {
-      res.sendHeader(404, {'Content-Type': 'text/plain'});
-      res.sendBody('Sorry, no paper this morning!');
-      res.finish();
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('Sorry, no paper this morning!');
+      res.close();
     });
   }
   else {
@@ -95,30 +96,30 @@ var requestHandler = function (clientRequest, clientResponse) {
     var c = http.createClient(uri.port, uri.hostname);
 
     //Stop from requesting gzip
-    clientRequest.headers['accept-encoding'] = "text/html";
+    req.headers['accept-encoding'] = "text/html";
 
-    var proxyRequest = c.request(clientRequest.method, pathname, clientRequest.headers);
+    var proxyRequest = c.request(req.method, pathname, req.headers);
     proxyRequest.addListener("response", function (response) {
-      clientResponse.writeHead(response.statusCode, response.headers);
+      res.writeHead(response.statusCode, response.headers);
       response.addListener("data", function (chunk) {
         // modify the html content
         if (response.headers['content-type'].indexOf("text/html") != -1) {
           if (chunk.toString().indexOf('</head>')) {
-            var includes =  '<script type="text/javascript" src="/jelly-serv/jquery-1.4.2.js"></script><script type="text/javascript" src="/jelly-serv/jelly.js"></script></head>';
+            var includes =  '<script type="text/javascript" src="/jelly-serv/jquery-1.4.2.js"></script><script type="text/javascript" src="/jelly-serv/jelly.js"></script><script type="text/javascript" src="/jelly-serv/wm.js"></script></head>';
             chunk = chunk.toString().replace('</head>', includes);
           }
         }
-        clientResponse.write(chunk, 'binary');
+        res.write(chunk, 'binary');
       })
       response.addListener("end", function () {
-        clientResponse.end();
+        res.end();
       })
     })
-    
-    clientRequest.addListener("data", function (chunk) {
+
+    req.addListener("data", function (chunk) {
       proxyRequest.write(chunk, 'binary');
     })
-    clientRequest.addListener("end", function () {
+    req.addListener("end", function () {
       proxyRequest.end();
     })
   }
@@ -130,22 +131,28 @@ sys.puts("Proxy Server running at http://127.0.0.1:8000/")
 
 //Service communication server
 http.createServer(function (req, res) {
-  req.addListener("data", function(chunk) {
-    eval('var wave='+chunk);
-    var assign = function(who, what) {
-      for (var nemato in tentacles) {
-        var obj = tentacles[nemato];
-        //needs to be expanded to match via url or title regex
-        if (obj.title.indexOf(who) != -1) {
-           obj.queue.push(what);
-           sys.puts('Assigned: '+what+' to: '+obj.title+', nemato: '+nemato);
-        }
-      }
-    }
-    //Assign what commands to execute on what tentacles
-    assign(wave.who, wave.what);
-  });
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Thanks\n');
+  var uri = url.parse(req.url);
+  if (uri.port == undefined) {
+    uri.port = {"http:":80,"https:":443}[uri.protocol]
+  }
+  var pathname = uri.search ? uri.pathname + uri.search : uri.pathname;
+
+  if (pathname.indexOf("assign") != -1) {
+    req.addListener("data", function(chunk) {
+        eval('var wave='+chunk);
+        //Assign what commands to execute on what tentacles
+        var resp = assign(wave.who, wave.what);
+        finish(req, res, resp);
+    });
+  }
+  else {
+    paperboy
+    .deliver(ADMIN, req, res)
+    .otherwise(function() {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('Sorry, no paper this morning!');
+      res.close();
+    });
+  }
 }).listen(8888);
 sys.puts('Service Server running at http://127.0.0.1:8888/');
--- static/jelly.js
+++ static/jelly.js
@@ -5,38 +5,44 @@ function run (data) {
 }
 
 function waitForMsg() {
-    $jellyQ.ajax({
-        type: "GET",
-        url: "/jelly-poll?tid="+window.jellyId,
-        async: true,
-        cache: true,
-        timeout:50000,
-        success: function(data){
-            run(data);            
-            setTimeout(
-                'waitForMsg()',
-                1000
-            );
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown){
-            setTimeout(
-                'waitForMsg()',
-                "15000");
-        },
-    });
+  $jellyQ.ajax({
+    type: "GET",
+    url: "/jelly-poll?tid="+window.jellyId,
+    async: true,
+    cache: true,
+    timeout:50000,
+    success: function(data){
+      run(data);      
+      setTimeout(
+        'waitForMsg()',
+        1000
+      );
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown){
+      setTimeout(
+        'waitForMsg()',
+        "15000");
+    },
+  });
 };
 
 $jellyQ(document).ready(function(){
   var data = {};
   data.title = window.document.title;
   data.url = window.location.href;
+  data.agent = navigator.userAgent;
   
   $jellyQ.post('/jelly-net/add', JSON.stringify(data), function(data) {
-    window.jellyId = data.id;
-    waitForMsg();
+  window.jellyId = data.id;
+  waitForMsg();
   }, 'json');
 });
 
+var oldbeforeunload = window.onbeforeunload;
 window.onbeforeunload = function() {
   $jellyQ.post('/jelly-net/rm?tid='+window.jellyId, window.jellyId, function(data){});
+  
+  if (oldbeforeunload) {
+    oldbeforeunload();
+  }
 };
